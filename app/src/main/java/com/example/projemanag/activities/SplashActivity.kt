4f3ac739bba8976662c2994340570560c74c645f
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-05-03 18:40:28 +0500

    Added -> auto login

--- app/src/main/java/com/example/projemanag/activities/SplashActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 import com.example.projemanag.databinding.ActivitySplashBinding
+import com.example.projemanag.firebase.FirestoreClass
 
 class SplashActivity : AppCompatActivity() {
     private var binding: ActivitySplashBinding? = null
@@ -24,7 +25,14 @@ class SplashActivity : AppCompatActivity() {
         binding!!.tvAppName.typeface  = typeFace
 
         Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
+
+            var currentUserID = FirestoreClass().getCurrentUserId()
+
+            if (currentUserID.isNotEmpty()){
+                startActivity(Intent(this, MainActivity::class.java))
+            }else {
+                startActivity(Intent(this, IntroActivity::class.java))
+            }
             finish()
         }, 2500)
     }
