210dbdf4d21e19d69a3ef503294f0ea89d0f1163
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-05-03 16:21:05 +0500

    Added -> registering the user via email in firebase

--- app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/SignUpActivity.kt
@@ -7,6 +7,8 @@ import android.view.WindowManager
 import android.widget.Toast
 import com.example.projemanag.R
 import com.example.projemanag.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUpActivity : BaseActivity() {
     private var binding : ActivitySignUpBinding? = null
@@ -45,11 +47,34 @@ class SignUpActivity : BaseActivity() {
         val password : String = binding!!.etPassword.text.toString().trim { it <= ' ' }
         
         if (validateForm(name, email, password)){
-            Toast.makeText(
-                this@SignUpActivity,
-                "Now we can register a new user.",
-                Toast.LENGTH_SHORT
-            ).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registerEmail = firebaseUser.email!!
+                        Toast.makeText(
+                            this,
+                            "$name you have successfully register the email address $registerEmail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            task.exception!!.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+//            Toast.makeText(
+//                this@SignUpActivity,
+//                "Now we can register a new user.",
+//                Toast.LENGTH_SHORT
+//            ).show()
         }
     }
 
