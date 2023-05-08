d3e68486d6175de184238e534a72df1260a1ba94
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-05-08 15:17:51 +0500

    Added -> displaying the cards

---
+++ app/src/main/java/com/example/projemanag/adapters/CardListItemsAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.projemanag.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projemanag.R
+import com.example.projemanag.models.Card
+import kotlinx.android.synthetic.main.item_card.view.*
+
+class CardListItemsAdapter(
+    private val context: Context,
+    private var list : ArrayList<Card>
+    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var onClickListener : OnClickListener? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_card,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder) {
+
+            holder.itemView.tv_card_name.text = model.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
