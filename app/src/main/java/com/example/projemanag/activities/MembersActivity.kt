ed7a88e494709e4c36facc01fabec25f119d4194
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-05-08 18:06:55 +0500

    Added -> new members - UI

--- app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/MembersActivity.kt
@@ -1,7 +1,12 @@
 package com.example.projemanag.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projemanag.R
 import com.example.projemanag.adapters.MemberListItemsAdapter
@@ -11,6 +16,7 @@ import com.example.projemanag.models.User
 import com.example.projemanag.utilis.Constants
 import kotlinx.android.synthetic.main.activity_members.*
 import kotlinx.android.synthetic.main.activity_task_list.*
+import kotlinx.android.synthetic.main.dialog_search_member.*
 
 class MembersActivity : BaseActivity() {
     private lateinit var mBoardDetails : Board
@@ -28,6 +34,25 @@ class MembersActivity : BaseActivity() {
             mBoardDetails.assignedTo
         )
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu to use in the action bar
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.action_add_member -> {
+
+                dialogSearchMember()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     fun setupMembersList(list: ArrayList<User>) {
 
@@ -51,5 +76,32 @@ class MembersActivity : BaseActivity() {
         }
 
         toolbar_members_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+
+    private fun dialogSearchMember() {
+        val dialog = Dialog(this)
+        /*Set the screen content from a layout resource.
+    The resource will be inflated, adding all top-level views to the screen.*/
+        dialog.setContentView(R.layout.dialog_search_member)
+        dialog.tv_add.setOnClickListener(View.OnClickListener {
+
+            val email = dialog.et_email_search_member.text.toString()
+
+            if (email.isNotEmpty()) {
+                dialog.dismiss()
+            } else {
+                Toast.makeText(
+                    this@MembersActivity,
+                    "Please enter members email address.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+        dialog.tv_cancel.setOnClickListener(View.OnClickListener {
+            dialog.dismiss()
+        })
+        //Start the dialog and display it on screen.
+        dialog.show()
     }
 }
