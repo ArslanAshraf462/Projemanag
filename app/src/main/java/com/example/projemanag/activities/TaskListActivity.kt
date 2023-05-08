4f9dfec4970ca8ee268355e05230fffccc92940e
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-05-08 11:28:34 +0500

    Added -> creating a list inside a board

--- app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/TaskListActivity.kt
@@ -12,6 +12,7 @@ import com.example.projemanag.utilis.Constants
 import kotlinx.android.synthetic.main.activity_task_list.*
 
 class TaskListActivity : BaseActivity() {
+    private lateinit var mBoardDetails : Board
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_list)
@@ -25,22 +26,23 @@ class TaskListActivity : BaseActivity() {
         FirestoreClass().getBoardDetails(this, boardDoucumentId)
     }
 
-    private fun  setupActionBar(title : String){
+    private fun  setupActionBar(){
         setSupportActionBar(toolbar_task_list_activity)
 
         val actionBar = supportActionBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
-            actionBar.title = title
+            actionBar.title = mBoardDetails.name
         }
 
         toolbar_task_list_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
     fun boardDetails(board: Board){
+        mBoardDetails = board
         hideProgressDialog()
-        setupActionBar(board.name)
+        setupActionBar()
         // Setup the task list view using the adapter class and task list of the board.
         // Here we are appending an item view for adding a list task list for the board.
         val addTaskList = Task(resources.getString(R.string.add_list))
@@ -53,5 +55,24 @@ class TaskListActivity : BaseActivity() {
         // Create an instance of TaskListItemsAdapter and pass the task list to it.
         val adapter = TaskListItemsAdapter(this@TaskListActivity, board.taskList)
         rv_task_list.adapter = adapter // Attach the adapter to the recyclerView.
+    }
+
+    fun addUpdateTaskListSuccess(){
+        hideProgressDialog()
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().getBoardDetails(this, mBoardDetails.documentId)
+    }
+
+    fun createTaskList(taskListName: String){
+        val task = Task(taskListName, FirestoreClass().getCurrentUserId())
+
+        mBoardDetails.taskList.add(0, task)
+        mBoardDetails.taskList.removeAt(mBoardDetails.taskList.size - 1)
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().addUpdateTaskList(this, mBoardDetails)
     }
 }
--- app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
@@ -57,7 +57,9 @@ class FirestoreClass {
             .addOnSuccessListener {
                     document ->
                 Log.i(activity.javaClass.simpleName, document.toString())
-                activity.boardDetails(document.toObject(Board::class.java)!!)
+                val board = document.toObject(Board::class.java)!!
+                board.documentId = document.id
+                activity.boardDetails(board)
             }
             .addOnFailureListener {
                     e ->
@@ -117,6 +119,24 @@ class FirestoreClass {
                     }
                 }
                 Log.e("SignInUser", "Error writing document", e)
+            }
+    }
+
+    fun addUpdateTaskList(activity: TaskListActivity, board: Board){
+        val taskListHashMap = HashMap<String, Any>()
+        taskListHashMap[Constants.TASK_LIST] = board.taskList
+
+        mFireStore.collection(Constants.BOARDS)
+            .document(board.documentId)
+            .update(taskListHashMap)
+            .addOnSuccessListener {
+                Log.e(activity.javaClass.simpleName, "TaskList updated successfully")
+                activity.addUpdateTaskListSuccess()
+            }
+            .addOnFailureListener {
+                exception ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName, "Error while creating a board", exception)
             }
     }
 
